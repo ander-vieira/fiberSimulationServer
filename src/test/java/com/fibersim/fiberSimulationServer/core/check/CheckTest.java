8dbfe39999f8166517c71add6c1a26b938debb6f
Ander Vieira <ander.vieira741@gmail.com>
2021-11-30 22:40:31 +0100

    Add unit tests for checks and phases

---
+++ src/test/java/com/fibersim/fiberSimulationServer/core/check/CheckTest.java
@@ -0,0 +1,24 @@
+package com.fibersim.fiberSimulationServer.core.check;
+
+import com.fibersim.fiberSimulationServer.core.util.Vector3;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CheckTest {
+    @Test
+    public void checkTest() {
+        Check cylinderCheck = new CylinderCheck(1);
+        Check cylinderCheck2 = new CylinderCheck(1, Vector3.X);
+        Check zIntervalCheck = new ZIntervalCheck(0, 1);
+
+        Assertions.assertTrue(Check.alwaysTrue.check(Vector3.O, Vector3.X));
+        Assertions.assertTrue(Check.alwaysTrue.check(Vector3.Z, Vector3.Y));
+        Assertions.assertTrue(cylinderCheck.check(Vector3.O, Vector3.X));
+        Assertions.assertFalse(cylinderCheck.check(new Vector3(1.0, 1.0, 1.0), Vector3.X));
+        Assertions.assertFalse(cylinderCheck2.check(new Vector3(0, 0.5, 0), Vector3.X));
+        Assertions.assertTrue(zIntervalCheck.check(new Vector3(1, 2, 0.5), Vector3.Z));
+        Assertions.assertFalse(zIntervalCheck.check(new Vector3(3, 0, 1.3), Vector3.Y));
+    }
+}
---
+++ src/test/java/com/fibersim/fiberSimulationServer/core/phase/PhaseTest.java
@@ -0,0 +1,30 @@
+package com.fibersim.fiberSimulationServer.core.phase;
+
+import com.fibersim.fiberSimulationServer.core.util.Vector3;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PhaseTest {
+    @Test
+    public void phaseTest() {
+        Phase planePhase = new PlanePhase(Vector3.Z, Vector3.O);
+        Phase cylinderPhase = new CylinderPhase(1, Vector3.O, Vector3.Z);
+
+        Vector3 vec1 = new Vector3(0.8, 0.6, 0.3);
+        Vector3 vec2 = new Vector3(0.6, 0.4, 0.8);
+        Vector3 vec3 = new Vector3(1.6, 0.6, 0.8);
+        Vector3 vec4 = new Vector3(-1, 0, 0);
+
+        Assertions.assertEquals(0, planePhase.getNormalVector(Vector3.X).minus(Vector3.Z).norm(), 1e-12);
+        Assertions.assertEquals(0, cylinderPhase.getNormalVector(vec1).minus(vec1.projectTangent(Vector3.Z)).norm(), 1e-12);
+
+        Assertions.assertEquals(0.8, planePhase.intersectionPoint(vec3, new Vector3(0, 0, -1)), 1e-12);
+        Assertions.assertEquals(Double.POSITIVE_INFINITY, planePhase.intersectionPoint(vec2, Vector3.Z), 1e-12);
+        Assertions.assertEquals(0.8, cylinderPhase.intersectionPoint(vec3, vec4), 1e-12);
+        Assertions.assertEquals(1, cylinderPhase.intersectionPoint(Vector3.O, vec4), 1e-12);
+        Assertions.assertEquals(Double.POSITIVE_INFINITY, cylinderPhase.intersectionPoint(vec3, Vector3.X));
+        Assertions.assertEquals(Double.POSITIVE_INFINITY, cylinderPhase.intersectionPoint(vec3, Vector3.Y));
+    }
+}
