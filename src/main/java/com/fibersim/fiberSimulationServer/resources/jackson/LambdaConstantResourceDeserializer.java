f15a38876935decfcfe75bc8fa781a748f01f876
Ander Vieira <ander.vieira741@gmail.com>
2021-12-03 01:57:42 +0100

    Add constant lambda function type (untested)

--- src/main/java/com/fibersim/fiberSimulationServer/resources/jackson/LambdaConstantResourceDeserializer.java
+++ src/main/java/com/fibersim/fiberSimulationServer/resources/jackson/LambdaConstantResourceDeserializer.java
@@ -4,15 +4,20 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.node.DoubleNode;
 import com.fibersim.fiberSimulationServer.resources.LambdaConstantResource;
+import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
 
+@JsonComponent
 public class LambdaConstantResourceDeserializer extends JsonDeserializer<LambdaConstantResource> {
     @Override
     public LambdaConstantResource deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         TreeNode treeNode = jsonParser.getCodec().readTree(jsonParser);
 
-        return null;
+        return LambdaConstantResource.builder()
+                .value(((DoubleNode)treeNode.get("value")).asDouble())
+                .build();
     }
 }
