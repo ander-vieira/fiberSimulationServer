f0153ae5ade17e92268b07f62e0da10b5046ee9a
Ander Vieira <ander.vieira741@gmail.com>
2021-12-04 18:34:54 +0100

    Optimize imports in test

--- src/test/java/com/fibersim/fiberSimulationServer/core/check/CheckTest.java
+++ src/test/java/com/fibersim/fiberSimulationServer/core/check/CheckTest.java
@@ -4,9 +4,7 @@ import com.fibersim.fiberSimulationServer.core.util.Vector3;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 public class CheckTest {
--- src/test/java/com/fibersim/fiberSimulationServer/core/util/MathUtilsTest.java
+++ src/test/java/com/fibersim/fiberSimulationServer/core/util/MathUtilsTest.java
@@ -3,9 +3,7 @@ package com.fibersim.fiberSimulationServer.core.util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 public class MathUtilsTest {
